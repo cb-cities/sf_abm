935b35dfb64eebc6b6ac419cd7f88e5867df3e10
Bingyu Zhao <bz247@login-e-13.data.cluster>
2018-06-09 01:06:07 +0100

    logging

--- sf_abm.py
+++ sf_abm.py
@@ -8,6 +8,7 @@ import scipy.stats
 #from multiprocessing import Pool 
 from itertools import repeat 
 import time
+import logging
 
 def chunks(vcount, n):
     for i in range(0, vcount, n+1):
@@ -15,11 +16,13 @@ def chunks(vcount, n):
 
 def map_edge_pop(vL, g, OD, graphID_dict):
     results = []
+    destination_counts = 0
     for origin in vL:
         ### Origin's ID on graph
         origin_graphID = graphID_dict[origin]
         ### Destination list's IDs on graph
         destination_list = OD.rows[origin]
+        destination_counts += len(destination_list)
         destination_graphID_list = [graphID_dict[d] for d in destination_list]
         ### Population traversing the OD
         population_list = OD.data[origin]
@@ -36,7 +39,7 @@ def map_edge_pop(vL, g, OD, graphID_dict):
             for di in range(len(path_collection)):
                 path_result = [(edge, population_list[di]) for edge in path_collection[di]]
                 results += path_result
-    return results
+    return results, destination_counts
 
 def edge_tot_pop(L):
     edge_volume = {}
@@ -46,7 +49,7 @@ def edge_tot_pop(L):
             edge_volume[p[0]] += p[1]
         except KeyError:
             edge_volume[p[0]] = p[1]
-    print('edges to be updated', len(edge_volume))
+    logger.debug('edges to be updated {}'.format(len(edge_volume)))
     return edge_volume
 
 def random_OD(g):
@@ -55,7 +58,6 @@ def random_OD(g):
     rvs = scipy.stats.poisson(25, loc=10).rvs
     OD_matrix = scipy.sparse.random(g.vcount(), g.vcount(), 
         density=2*10e-6, format='csr', data_rvs=rvs)
-    print('no. of OD pairs', len(OD_matrix.data))
     OD_matrix = OD_matrix.tolil()
     return OD_matrix
 
@@ -65,22 +67,22 @@ def one_step(g, day, hour):
     ### Read/Generate OD matrix for this time step
     #OD_matrix = random_OD(g)
     OD_matrix = scipy.sparse.load_npz('TNC/OD_matrices/DY{}_HR{}_OD.npz'.format(day, hour))
-    print('finish reading sparse OD matrix')
-    print(OD_matrix.shape)
+    logger.debug('finish reading sparse OD matrix, shape is {}'.format(OD_matrix.shape))
     OD_matrix = OD_matrix.tolil()
-    print('finish converting the matrix to lil')
+    logger.info('finish converting the matrix to lil')
     ### Load the dictionary used to find the osm_node_id from matrix row/col id
     OD_nodesID_dict = json.load(open('TNC/OD_matrices/DY{}_HR{}_node_dict.json'.format(day, hour)))
-    print('finish loading nodesID_dict')
+    logger.info('finish loading nodesID_dict')
     g_vs_node_osmid = g.vs['node_osmid']
-    print('finish g_vs_node_osmid')
+    logger.info('finish g_vs_node_osmid')
     g_vs_node_osmid_dict = {g_vs_node_osmid[i]: i for i in range(g.vcount())}
-    print('finish g_vs_node_osmid_dict')
+    logger.info('finish g_vs_node_osmid_dict')
     OD_graphID_dict = {int(key): g_vs_node_osmid_dict[value] for key, value in OD_nodesID_dict.items()}
-    print('finish converting OD matrix id to graph id')
+    logger.info('finish converting OD matrix id to graph id')
 
     ### Partition the nodes into 4 chuncks
-    vcount = 400 #OD_matrix.shape[0]
+    vcount = 4 #OD_matrix.shape[0]
+    logger.debug('number of origins {}'.format(vcount))
     # process_count = 1
     # partitioned_v = list(chunks(vcount, int(vcount/process_count)))
     # print('vertices partition finished')
@@ -91,7 +93,8 @@ def one_step(g, day, hour):
 
     ### Generate (edge, population) tuple
     #edge_pop_tuples = pool.starmap(map_edge_pop, zip(partitioned_v, repeat(g), repeat(OD_matrix), repeat(OD_graphID_dict)))
-    edge_pop_tuples = map_edge_pop(range(vcount), g, OD_matrix, OD_graphID_dict)
+    edge_pop_tuples, destination_counts = map_edge_pop(range(vcount), g, OD_matrix, OD_graphID_dict)
+    logger.debug('number of destinations {}'.format(destination_counts))    
 
     ### Close the pool
     # pool.close()
@@ -107,14 +110,14 @@ def one_step(g, day, hour):
 def main():
     ### Read initial graph
     g = igraph.load('data_repo/Imputed_data_False9_0509.graphmlz')
-    print(g.summary())
+    logger.debug('graph summary {}'.format(g.summary()))
     g.es['weights'] = g.es['sec_length']
-    print('graph weights attribute created')
+    logger.info('graph weights attribute created')
 
     t0 = time.time()
     edge_volume = one_step(g, 1, 3)
     t1 = time.time()
-    print('running time is {}'.format(t1-t0))
+    logger.debug('running time for one time step is {}'.format(t1-t0))
     ### Update graph
     edge_weights = np.array(g.es['weights'])
     edge_weights[list(edge_volume.keys())] = np.array(list(edge_volume.values()))
@@ -122,8 +125,11 @@ def main():
 
 
 if __name__ == '__main__':
-    t0 = time.time()
+    logging.basicConfig(filename='sf_abm.log', level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.debug('Current time {}'.format(time.strftime('%Y-%m-%d %H:%M')))
+    t_start = time.time()
     main()
-    t1 = time.time()
-    print('total run time is {} seconds'.format(t0-t1))
+    t_end = time.time()
+    logger.debug('total run time is {} seconds'.format(t_end-t_start))
 
