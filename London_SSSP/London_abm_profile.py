53a20861494bc3040226c01c3204281498bb02e3
bz247 <bz247@cam.ac.uk>
2018-09-10 17:29:54 -0700

    Remove London_SSSP from repo

--- London_SSSP/London_abm_profile.py
+++
@@ -1,5 +0,0 @@
-import cProfile
-import London_abm_mp_sssp_full
-
-cProfile.run('London_abm_mp_sssp_full.main()', 'London_abm_mp_sssp_profile.txt')
-#cProfile.run('mp_test.main()', 'mp_test_profile.txt')
