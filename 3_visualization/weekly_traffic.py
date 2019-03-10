605108240c960aff4ecb73449566dac800383f69
bz247 <bz247@cam.ac.uk>
2019-03-10 10:02:33 +0900

    calculate seven day traffic

--- 3_visualization/weekly_traffic.py
+++ 3_visualization/weekly_traffic.py
@@ -12,35 +12,40 @@ folder = 'sf_overpass/original'
 
 plt.rcParams.update({'font.size': 15, 'font.weight': "normal", 'font.family':'serif', 'axes.linewidth': 0.1})
 
-def main():
+def weekly_traffic(case):
 
     ### Aggregate hourly flow to weekly flow
     network_attr_df = pd.read_csv(absolute_path+'/../0_network/data/{}/edges_elevation.csv'.format(folder))
     network_attr_df = network_attr_df[['edge_id_igraph', 'length', 'geometry']]
-    network_attr_df['fiveday_traffic'] = 0
+    network_attr_df[case] = 0
 
     random_seed = 0
     probe_ratio = 1
-    for day in [0, 1, 2, 3, 4]:
+    if case=='sevenday_traffic':
+        day_list = [0, 1, 2, 3, 4, 5, 6]
+    else:
+        day_list = [0, 1, 2, 3, 4]
+
+    for day in day_list:
         for hour in range(3, 27):
             hour_edge_flow_df = pd.read_csv(absolute_path+'/../2_ABM/output/edges_df/edges_df_DY{}_HR{}_r{}_p{}.csv'.format(day, hour, random_seed, probe_ratio))
-            network_attr_df = pd.merge(network_attr_df, hour_edge_flow_df[['edge_id_igraph', 'net_true_flow']], on = ['edge_id_igraph'])
-            network_attr_df['fiveday_traffic'] += network_attr_df['net_true_flow']
-            network_attr_df = network_attr_df[['edge_id_igraph', 'length', 'fiveday_traffic']]
+            network_attr_df = pd.merge(network_attr_df, hour_edge_flow_df[['edge_id_igraph', 'true_flow']], on = ['edge_id_igraph'])
+            network_attr_df[case] += network_attr_df['true_flow']
+            network_attr_df = network_attr_df[['edge_id_igraph', 'length', case]]
 
     ### Directed flow
-    edge_flow_df.to_csv('directed_weekly_traffic_20190227.csv', index=False)
+    edge_flow_df.to_csv('directed_{}.csv'.format(case), index=False)
 
     ### Merge results from two directions
     edge_flow_df['edge_id_igraph_str'] = edge_flow_df['edge_id_igraph'].astype(str)
     edge_flow_df['undir_uv_igraph'] = pd.DataFrame(np.sort(edge_flow_df[['start_igraph', 'end_igraph']].values, axis=1), columns=['small_igraph', 'large_igraph']).apply(lambda x:'%s_%s' % (x['small_igraph'],x['large_igraph']),axis=1)
     edge_flow_df_grp = edge_flow_df.groupby('undir_uv_igraph').agg({
-            'fiveday_traffic': np.sum, 
+            case: np.sum, 
             'edge_id_igraph_str': lambda x: '-'.join(x),
             'geometry': 'first'}).reset_index()
-    edge_flow_df_grp = edge_flow_df_grp.rename(columns={'fiveday_traffic': 'undirected_fiveday_traffic'})
+    edge_flow_df_grp = edge_flow_df_grp.rename(columns={case: 'undirected_{}'.format(case)})
 
-    edge_flow_df_grp.to_csv('undirected_weekly_traffic_20190227.csv', index=False)
+    edge_flow_df_grp.to_csv('undirected_{}.csv'.format(case), index=False)
 
 def delay_time():
 
@@ -84,8 +89,8 @@ def delay_time():
 
 
 if __name__ == '__main__':
-    #main()
-    delay_time()
+    weekly_traffic('sevenday_traffic')
+    #delay_time()
 
 
 
--- 6_sensor/plot.py
+++ 6_sensor/plot.py
@@ -15,22 +15,91 @@ plt.rcParams.update({'font.size': 18, 'font.weight': "normal", 'font.family':'se
 folder = 'sf_overpass'
 scenario = 'original'
 
-def plot_hourly_trend(var):
+def plot_peak_hour(var, hour):
 
-    fig, ax = plt.subplots(1)
-    fig.set_size_inches(10.5, 5.5)
-    color = iter(cm.rainbow(np.linspace(0, 1, 6)))
-    for probe_ratio in [1.0, 0.1, 0.01, 0.005, 0.001, 0.0]:
-
-        c = next(color)
+    plot_df = pd.DataFrame(columns=['probe_ratio', 'cov', 'random_seed', 'AMT'])
+    for probe_ratio in [1.0, 0.1, 0.01, 0.005]:
+        ### COV 0 case
         summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor/summary_df/summary_p{}.csv'.format(probe_ratio))
         summary_df['VHT'] = summary_df['VHT']/3600
         summary_df['VKMT'] = summary_df['VKMT']/1000
+        summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
+        summary_df['cov'] = 0.0
+
+        plot_df = pd.concat([plot_df, summary_df.loc[summary_df['hour']==hour, ['probe_ratio', 'cov', 'random_seed', 'AMT']]])
+
+        for cov in [0.5, 1.0, 2.0]:
+            summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor_cov/summary_df/summary_p{}_cov{}.csv'.format(probe_ratio, cov))
+            summary_df['VHT'] = summary_df['VHT']/3600
+            summary_df['VKMT'] = summary_df['VKMT']/1000
+            summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
+            summary_df['cov'] = cov
+
+            plot_df = pd.concat([plot_df, summary_df.loc[summary_df['hour']==hour, ['probe_ratio', 'cov', 'random_seed', 'AMT']]])
+
+    plot_df['probe_ratio_str'] = plot_df['probe_ratio'].astype(str)
+    fig, ax = plt.subplots(1)
+    fig.set_size_inches(10.5, 5.5)
+    color = iter(cm.viridis(np.linspace(0, 1, 4)))
+
+    for cov in [0.0, 0.5, 1.0, 2.0]:
+        c=next(color)
+        slice_df = plot_df.loc[plot_df['cov']==cov].copy()
+        slice_grp = slice_df.groupby('probe_ratio_str').agg({var: [np.mean, np.std]}).reset_index()
+        slice_grp.columns = ['_'.join(col).strip() for col in slice_grp.columns.values]
+
+        ax.plot('probe_ratio_str_', '{}_mean'.format(var), data=slice_grp, label='{}'.format(cov), c=c)
+        ax.fill_between(slice_grp['probe_ratio_str_'], 
+            slice_grp['{}_mean'.format(var)] - slice_grp['{}_std'.format(var)],
+            slice_grp['{}_mean'.format(var)] + slice_grp['{}_std'.format(var)],
+            facecolor=c, alpha=0.5)
+
+    ### Shrink current axis's height
+    box = ax.get_position()
+    #ax.set_position([box.x0, box.y0+box.height*0.2, box.width, box.height*0.9])
+    ax.set_position([box.x0, box.y0, box.width*0.9, box.height])
+
+    ax.set_title("Average agent travel time (Friday {} o'clock)".format(hour), y=1.05)
+    ax.legend(title='Probe info\nvariability\n(coef. of\nvariation)', bbox_to_anchor=(1, 0.8))
+    ax.set_xlabel('Probe ratio')
+    #plt.xscale('log')
+    ax.set_ylabel('Average agent travel time (min)')
+    #ax.set_ylim([30, 42])
+    #plt.yscale('log')
+    plt.savefig(absolute_path+'/Figs/{}oc_{}.png'.format(hour, var))
+    #plt.show()
+
+
+def plot_hourly_amt(var, probe_ratio):
+
+    fig, ax = plt.subplots(1)
+    fig.set_size_inches(10.5, 5.5)
+    color = iter(cm.viridis(np.linspace(0, 1, 4)))
+
+    ### COV 0 case
+    summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor/summary_df/summary_p{}.csv'.format(probe_ratio))
+    summary_df['VHT'] = summary_df['VHT']/3600
+    summary_df['VKMT'] = summary_df['VKMT']/1000
+    summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
+    summary_grp = summary_df.groupby('hour').agg({var: [np.mean, np.std]}).reset_index()
+    summary_grp.columns = ['_'.join(col).strip() for col in summary_grp.columns.values]
+
+    #summary_grp = summary_grp.loc[(summary_grp['hour_']>10) & (summary_grp['hour_']<24)].copy()
+    c = next(color)
+    ax.plot('hour_', '{}_mean'.format(var), data=summary_grp, label='0', c=c)
+
+    for cov in [0.5, 1.0, 2.0]:
+
+        c = next(color)
+        summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor_cov/summary_df/summary_p{}_cov{}.csv'.format(probe_ratio, cov))
+        summary_df['VHT'] = summary_df['VHT']/3600
+        summary_df['VKMT'] = summary_df['VKMT']/1000
+        summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
         summary_grp = summary_df.groupby('hour').agg({var: [np.mean, np.std]}).reset_index()
         summary_grp.columns = ['_'.join(col).strip() for col in summary_grp.columns.values]
 
-        summary_grp = summary_grp.loc[(summary_grp['hour_']>10) & (summary_grp['hour_']<24)].copy()
-        ax.plot('hour_', '{}_mean'.format(var), data=summary_grp, label='{}'.format(probe_ratio), c=c)
+        #summary_grp = summary_grp.loc[(summary_grp['hour_']>10) & (summary_grp['hour_']<24)].copy()
+        ax.plot('hour_', '{}_mean'.format(var), data=summary_grp, label='{}'.format(cov), c=c)
         ax.fill_between(summary_grp['hour_'], 
             summary_grp['{}_mean'.format(var)] - summary_grp['{}_std'.format(var)],
             summary_grp['{}_mean'.format(var)] + summary_grp['{}_std'.format(var)],
@@ -42,12 +111,68 @@ def plot_hourly_trend(var):
     #ax.set_position([box.x0, box.y0+box.height*0.2, box.width, box.height*0.9])
     ax.set_position([box.x0, box.y0, box.width*0.9, box.height])
 
-    plt.yscale('log')
-    ax.set_title('Hourly {} (Friday)'.format(var))
-    ax.legend(title='Probe ratio', bbox_to_anchor=(1, 0.7))
+    ax.set_title('Hourly average agent travel time (Friday), Probe ratio {}'.format(probe_ratio), y=1.05)
+    ax.legend(title='Probe coef.\nof variation', bbox_to_anchor=(1, 0.7))
     ax.set_xlabel('Hour')
-    ax.set_ylabel('{}'.format(var))
-    plt.savefig(absolute_path+'/Figs/hourly_{}.png'.format(var))
+    ax.set_ylabel('Average agent travel time (min)')
+    ax.set_ylim([15, 42])
+    plt.savefig(absolute_path+'/Figs/hourly_{}_p{}_test.png'.format(var, probe_ratio))
+    #plt.show()
+
+def plot_hourly_trend(var, cov, ylabel, zoom=False):
+
+    fig, ax = plt.subplots(1)
+    fig.set_size_inches(10.5, 5.5)
+    color = iter(cm.rainbow(np.linspace(0, 1, 6)))
+    if zoom == False: 
+        probe_ratio_list = [1.0, 0.1, 0.01, 0.005, 0.001, 0.0]
+    else: 
+        probe_ratio_list = [1.0, 0.1, 0.01, 0.005]
+    for probe_ratio in probe_ratio_list:
+
+        c = next(color)
+        # summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor_cov/summary_df/summary_p{}_cov{}.csv'.format(probe_ratio, cov))
+        # summary_df['VHT'] = summary_df['VHT']/3600
+        # summary_df['VKMT'] = summary_df['VKMT']/1000
+        # summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
+        # summary_grp = summary_df.groupby('hour').agg({var: [np.mean, np.std]}).reset_index()
+        # summary_grp.columns = ['_'.join(col).strip() for col in summary_grp.columns.values]
+
+        # #summary_grp = summary_grp.loc[(summary_grp['hour_']>10) & (summary_grp['hour_']<24)].copy()
+        # ax.plot('hour_', '{}_mean'.format(var), data=summary_grp, label='{}'.format(probe_ratio), c=c)
+        # ax.fill_between(summary_grp['hour_'], 
+        #     summary_grp['{}_mean'.format(var)] - summary_grp['{}_std'.format(var)],
+        #     summary_grp['{}_mean'.format(var)] + summary_grp['{}_std'.format(var)],
+        #     facecolor=c, alpha=0.5)
+        # #plt.errorbar('hour_', '{}_mean'.format(var), '{}_std'.format(var), data=summary_grp, fmt='.-', lw=0.5, color=c, elinewidth=2, ecolor=c, capsize=10, capthick=1)
+
+        ### COV 0 case
+        summary_df = pd.read_csv(absolute_path+'/../2_ABM/output/sensor/summary_df/summary_p{}.csv'.format(probe_ratio, cov))
+        summary_df['VHT'] = summary_df['VHT']/3600
+        summary_df['VKMT'] = summary_df['VKMT']/1000
+        summary_df['AMT'] = summary_df['VHT']/summary_df['hour_demand']*60 ### Agent minutes travelled
+        summary_grp = summary_df.groupby('hour').agg({var: [np.mean, np.std]}).reset_index()
+        summary_grp.columns = ['_'.join(col).strip() for col in summary_grp.columns.values]
+
+        #summary_grp = summary_grp.loc[(summary_grp['hour_']>10) & (summary_grp['hour_']<24)].copy()
+        ax.plot('hour_', '{}_mean'.format(var), data=summary_grp, label='{}%'.format(probe_ratio*100), c=c, linestyle='-')
+        ax.fill_between(summary_grp['hour_'], 
+            summary_grp['{}_mean'.format(var)] - summary_grp['{}_std'.format(var)],
+            summary_grp['{}_mean'.format(var)] + summary_grp['{}_std'.format(var)],
+            facecolor=c, alpha=0.5)
+    
+    ### Shrink current axis's height
+    box = ax.get_position()
+    #ax.set_position([box.x0, box.y0+box.height*0.2, box.width, box.height*0.9])
+    ax.set_position([box.x0-box.width*0.03, box.y0, box.width*0.87, box.height])
+
+    plt.yscale('log')
+    ax.set_title('Hourly {} (Friday), COV {}'.format(ylabel, cov), y=1.05)
+    l = ax.legend(title='Probe\npenetration\nrate', bbox_to_anchor=(1, 0.85))
+    plt.setp(l.get_title(), multialignment='center')
+    ax.set_xlabel('Hour')
+    ax.set_ylabel('{}'.format(ylabel))
+    plt.savefig(absolute_path+'/Figs/hourly_{}_cov{}_zoom{}.png'.format(var, cov, zoom))
     #plt.show()
 
 def plot_pcp(var, hour, random_seed):
@@ -99,5 +224,7 @@ def plot_pcp(var, hour, random_seed):
 
 
 if __name__ == '__main__':
-    #plot_hourly_trend('max10')
-    plot_pcp('true_flow', 3, 0) ### max flow hour is 18
+    #plot_peak_hour('AMT', 18)
+    #plot_hourly_amt('AMT', 1.0)
+    plot_hourly_trend('AMT', 0.0, 'Average Agent Travel Time (min)', zoom=False)
+    #plot_pcp('true_flow', 3, 0) ### max flow hour is 18
